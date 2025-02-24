4fe68a89e8d2ad27e9ecc0980e25e70ef019502e
asifm.ashique <asifm.ashique@petronas.com>
2025-02-24 17:23:11 +0600

    auth done

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
-import { cn } from '@/lib/utils';
+import { cn, getInitials } from '@/lib/utils';
 import { usePathname } from 'next/navigation';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Session } from 'next-auth';
 
-const Header = () => {
+const Header = ({ session }: { session: Session }) => {
   const pathname = usePathname();
   return (
     <header className='my-10 flex justify-between gap-5'>
@@ -24,6 +26,15 @@ const Header = () => {
             )}
           >
             Library
+          </Link>
+        </li>
+        <li>
+          <Link href='/my-profile'>
+            <Avatar>
+              <AvatarFallback className=' bg-amber-100'>
+                {getInitials(session?.user?.name || 'In')}
+              </AvatarFallback>
+            </Avatar>
           </Link>
         </li>
         <li>
